969aaff4e9bdb2d8dc805ee925737310cb5f2686
ofirsharony <ofir.sharony@myheritage.com>
2024-10-23 19:02:11 +0300

    add basic functionality

--- prompt-generator.py
+++ prompt-generator.py
@@ -1,45 +1,52 @@
 import os
+import json
+from openai import OpenAI
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    SystemMessage
-)
 
 # instructions
 # Store your OpenAI key in env var OPENAI_API_KEY
 # pip install openai langchain streamlit
-# streamlit run single-question-llm.py
+# streamlit run prompt-generator.py
 
-PROMPT = '''You are a professional storyteller. You will come up with entertaining stories that are engaging, imaginative, and captivating for the audience. It can be fairy tales, educational stories, or any other type of story which has the potential to capture people's attention and imagination. Depending on the target audience, you may choose specific themes or topics for your storytelling session e.g., if it’s children then you can talk about animals; If it’s adults then history-based tales might engage them better, etc.
----
-Story title: {} Main character: {} Target audience: {}'''
+# Load configuration from JSON file
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
 
+# Extract prompt from the configuration
+TEXT_OUTPUT_PROMPT = config["text_meta_prompt"].strip()
+AUDIO_OUTPUT_PROMPT = config["audio_meta_prompt"].strip()
+
+client = OpenAI()
 openai_api_key = os.environ.get('OPENAI_API_KEY')
-st.set_page_config(layout="centered", page_title="LLM stories generator")
-st.markdown("## LLM Stories Generator")
+
+st.set_page_config(layout="centered", page_title="Prompt Generator")
+st.markdown("## Generate the perfect prompt for your AI model")
 st.markdown("### Personalized stories for the masses")
 
-def generate_response(input_text, model_name):
-    chat = ChatOpenAI(temperature=0.3, openai_api_key=openai_api_key, model_name=model_name)
-    messages = [
-        SystemMessage(content=input_text),
-        # HumanMessage(content="Hi AI, how are you today?"),
-        # AIMessage(content="I'm great thank you. How can I help you?"),
-        # HumanMessage(content="I'd like to understand string theory.")
-    ]
-
-    return chat(messages)
-
+def generate_response(meta_prompt, task_or_prompt, model):
+    return client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": meta_prompt,
+            },
+            {
+                "role": "user",
+                "content": "Task, Goal, or Current Prompt:\n" + task_or_prompt,
+            },
+        ],
+    )
 
 with st.form('my_form'):
-    open_ai_model = st.selectbox('Which OpenAI model should we use?', ('gpt-3.5-turbo-16k', 'gpt-4', 'gpt-3.5-turbo'))
-    story_title = st.text_input("Story title", value="The author and the wonder")
-    main_character = st.text_input("Main character", value="Mia, an inspiring author, searching meaning and purpose through a life full of grit and perseverance")
-    target_audience = st.selectbox("Target audience",  ('Children', 'Students', 'Adults'))
-    prompt = st.text_area('Prompt:', value=PROMPT, height=220)
-    submitted = st.form_submit_button('Generate')
+    open_ai_model = st.selectbox('Which OpenAI model should we use?', ('gpt-4o', 'gpt-4o-mini', 'o1-preview', 'o1-mini'))
+    selected_option = st.radio("Choose the output format:", ['Text prompt', 'Audio prompt'])
 
+    selected_meta_prompt = TEXT_OUTPUT_PROMPT if selected_option == 'Text prompt' else AUDIO_OUTPUT_PROMPT
+    chosen_meta_prompt = st.text_area('Meta Prompt:', value=selected_meta_prompt, height=220)
+    task_goal_or_prompt = st.text_area('Task, Goal or Prompt:', value="", height=400)
+
+    submitted = st.form_submit_button('Generate')
     if submitted:
         with st.spinner('Generating LLM response...'):
-            narrative = generate_response(prompt.format(story_title, main_character, target_audience), open_ai_model)
-            st.info(narrative.content)
+            st.info(generate_response(chosen_meta_prompt, task_goal_or_prompt, open_ai_model).content)

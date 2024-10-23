32578cb083e4fe34073f24cdd1e399ba5787b923
ofirsharony <ofir.sharony@myheritage.com>
2024-10-23 19:21:49 +0300

    beautify and code cleanup

--- prompt-generator.py
+++ prompt-generator.py
@@ -20,8 +20,8 @@ client = OpenAI()
 openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 st.set_page_config(layout="centered", page_title="Prompt Generator")
-st.markdown("## Generate the perfect prompt for your AI model")
-st.markdown("### Personalized stories for the masses")
+st.markdown("## Prompt Generator")
+st.markdown("### Generate the perfect prompt for your AI model")
 
 def generate_response(meta_prompt, task_or_prompt, model):
     return client.chat.completions.create(
@@ -39,23 +39,20 @@ def generate_response(meta_prompt, task_or_prompt, model):
     )
 
 def update_meta_prompt():
-    selected_option = st.session_state["selected_option"]  # Access selected option from session state
-    chosen_meta_prompt_value = TEXT_OUTPUT_PROMPT if selected_option == 'Text prompt' else AUDIO_OUTPUT_PROMPT
+    st.session_state["selected_meta_prompt"] = TEXT_OUTPUT_PROMPT if st.session_state["selected_option"] == 'Text prompt' else AUDIO_OUTPUT_PROMPT
 
-# Place the radio button outside the form to avoid the Streamlit error
+# Place the radio button outside the form to avoid the Streamlit error, as a change state method is triggered
 selected_option = st.radio("Choose the output format:", ['Text prompt', 'Audio prompt'], key="selected_option", on_change=update_meta_prompt)
-chosen_meta_prompt_value = TEXT_OUTPUT_PROMPT if selected_option == 'Text prompt' else AUDIO_OUTPUT_PROMPT
+update_meta_prompt()
 
 with st.form('my_form'):
     open_ai_model = st.selectbox('Which OpenAI model should we use?', ('gpt-4o', 'gpt-4o-mini', 'o1-preview', 'o1-mini'))
 
     # Adjust chosen_meta_prompt based on session state logic
-    chosen_meta_prompt = st.text_area('Meta Prompt:', value=chosen_meta_prompt_value, height=220)  # Initial value can be empty
-
+    chosen_meta_prompt = st.text_area('Meta Prompt:', value=st.session_state["selected_meta_prompt"], height=220)  # Initial value can be empty
     task_goal_or_prompt = st.text_area('Task, Goal or Prompt:', value="", height=400)
 
     submitted = st.form_submit_button('Generate')
-
     if submitted:
         with st.spinner('Generating LLM response...'):
             st.info(generate_response(chosen_meta_prompt, task_goal_or_prompt, open_ai_model).content)

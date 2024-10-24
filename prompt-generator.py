a5e98fc0dd69f5eced0b5c6b4e93ae65853647bd
ofirsharony <ofir.sharony@myheritage.com>
2024-10-24 06:54:28 +0300

    final touches, remove langchain

--- prompt-generator.py
+++ prompt-generator.py
@@ -7,7 +7,6 @@ import streamlit as st
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
-# Extract prompt from the configuration
 TEXT_OUTPUT_PROMPT = config["text_meta_prompt"].strip()
 AUDIO_OUTPUT_PROMPT = config["audio_meta_prompt"].strip()
 
@@ -39,15 +38,15 @@ def update_meta_prompt():
     st.session_state["selected_meta_prompt"] = TEXT_OUTPUT_PROMPT if st.session_state["selected_option"] == 'Text prompt' else AUDIO_OUTPUT_PROMPT
 
 # Place the radio button outside the form to avoid the Streamlit error, as a change state method is triggered
-selected_option = st.radio("Choose the output format:", ['Text prompt', 'Audio prompt'], key="selected_option", on_change=update_meta_prompt)
+st.radio("Choose the output format:", ['Text prompt', 'Audio prompt'], key="selected_option", on_change=update_meta_prompt)
 update_meta_prompt()
 
 with st.form('my_form'):
     open_ai_model = st.selectbox('Which OpenAI model should we use?', ('gpt-4o', 'gpt-4o-mini', 'o1-preview', 'o1-mini'))
 
     # Adjust chosen_meta_prompt based on session state logic
-    chosen_meta_prompt = st.text_area('Meta Prompt:', value=st.session_state["selected_meta_prompt"], height=400)  # Initial value can be empty
-    task_goal_or_prompt = st.text_area('Task, Goal or Prompt:', value="", height=300)
+    chosen_meta_prompt = st.text_area('Meta Prompt:', value=st.session_state["selected_meta_prompt"], height=400)
+    task_goal_or_prompt = st.text_area('Task, Goal or Prompt:', value="Set the stage for a new show", height=300)
 
     submitted = st.form_submit_button('Generate')
     if submitted:
